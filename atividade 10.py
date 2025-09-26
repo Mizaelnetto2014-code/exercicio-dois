0339daeebd319e1c948a613aa9948607d08ff0a1
Mizael Queiroz <Mizaelnetto.2014@hotmail.com>
2025-09-26 17:47:06 -0300

    Add files via upload

---
+++ atividade 10.py
@@ -0,0 +1,5 @@
+nome=input('informe seu nome')
+idade=int(input('digite sua idade'))
+curso=input('informe seu curso')
+print(f"confirmado! {nome}, {idade},{curso} ,{nome},está inscrito no evento Feira de Ciência e Tecnologia. ")
+
